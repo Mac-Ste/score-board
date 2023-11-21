38d301a57600210592cf6a76b42ffacb04ff7442
macste <maciej.sterma@gmail.com>
2023-11-21 20:49:26 +0100

    Adding dependencies for connecting to PostgreSQL using Kotlin Exposed

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,11 +17,24 @@ repositories {
 
 dependencies {
     val logbackVersion = "1.4.11"
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    val exposedVersion = "0.44.1"
     implementation("ch.qos.logback:logback-classic:$logbackVersion")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
+    implementation("org.springframework.boot:spring-boot-starter-web")
     runtimeOnly("org.springframework.boot:spring-boot-devtools")
+
+    implementation("org.postgresql:postgresql:42.7.0")
+
+    // Exposed
+    implementation("org.jetbrains.exposed:exposed-core:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-crypt:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-dao:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-jdbc:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-kotlin-datetime:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-json:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-money:$exposedVersion")
+    implementation("org.jetbrains.exposed:exposed-spring-boot-starter:$exposedVersion")
 
     // TEST
     val kotestVersion = "5.8.0"
@@ -30,6 +43,7 @@ dependencies {
     testImplementation("io.kotest:kotest-runner-junit5:$kotestVersion")
     testImplementation("io.kotest:kotest-assertions-core:$kotestVersion")
     testImplementation("io.mockk:mockk:$mockkVersion")
+    testImplementation("org.testcontainers:postgresql:1.19.3")
 }
 
 tasks.test {
